92882dd6a1e24641f27ff94cdeaffe78b9b696e7
RosslanKoulli <rosslan.koulli@gmail.com>
2023-12-05 19:28:49 +0000

    Finish step 4 which allows us to spin the machine and give out a set of 9 different values on the slot machine which also include the global values.

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 // 1. Deposit some money
 // 2. Determine number of lines to bet on
 // 3. Collect a bet amount
-// 4. SPin the slot machine
+// 4. Spin the slot machine
 // 5. Check if the user won
 // 6. Give the user their winnings
 // 7. Play again 
@@ -9,6 +9,25 @@
 
 //STEP 1
 const prompt = require("prompt-sync")();
+
+//STEP 4
+const ROWS = 3; 
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+const SYMBOL_VALUES = {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+}
+//End of STEP 4
 
 const deposit = () => {
     while(true){
@@ -41,6 +60,7 @@ const getNumberOfLInes = () => {
     } 
 };
 
+
 //STEP 3
 const getBet = (balance, lines) => {
     while(true){
@@ -58,8 +78,32 @@ const getBet = (balance, lines) => {
 };
 
 
+//STEP 4
+const spin = () => {
+    const symbols = []; //We dont need to make it a let since we are not changing the value of the array, in javascript an array is an reference data type which means I can manipulate whats inside of the array without changeing the reference of the array so we can use const and still append the list.
+    for(const[symbol,count] of Object.entries(SYMBOLS_COUNT)){
+        for(let i = 0; i < count; i++){
+            symbols.push(symbol);             //push is the js version of pythons append
+        }
+    }
+    
+    const reels = [[],[],[]];
+    for(let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols];
+        for(let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length); // `Math.floor` rounds numbers down since indexs can only be whole nubmers
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1); //.splice removes n (1) element from the array with random index
 
 
+        }
+    }
+    return reels;
+};
+//End of STEP 4
+
+console.log(spin());
 let balance = deposit();    //Changing to let allows us to change the values of the varaibles since not constant `const`
 const numberOfLines = getNumberOfLInes();
 const bet = getBet(balance, numberOfLines );
